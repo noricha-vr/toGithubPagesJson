8d8b2ae23f33f9d14aef5d8f8f378c43597ef659
noricha-vr <noricha.vr@gmail.com>
2023-09-14 11:48:52 +0900

    Change url to environmental variables.

--- fetch_data.py
+++ fetch_data.py
@@ -1,10 +1,9 @@
 import requests
 import json
-
-
+import os
 if __name__ == '__main__':
-    # 指定されたURL
-    url = 'https://script.googleusercontent.com/macros/echo?user_content_key=eXBNGH5vxSbknxFPu0XdCHVJ_-ylFAAJez3-qsTD0Bqa6l_9C_-2AnuIhhJ2yyC1_rH0GpKfR0GuJLoyJ0jg9usnCdBwXs0ym5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnN9ca9CEOuXBb9p37hhwT2PaSCyLnbWkW-356mZJYYpLUPBy31-xkM-8AZcRbgUJK2syXgBr3L9Ti5fQSKjhzOadz_ORPUypytz9Jw9Md8uu&lib=M0Pe-JIfEhKXuDG58t6bPxj8rQg-QTqZL'
+    # 環境変数URLからURLを取得する
+    url = os.environ.get('URL')
     # URLからデータを取得する
     response = requests.get(url)
 
